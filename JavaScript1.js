e89b4bc0c828cb62979760ee68a5522d656c0e53
bobbiiyt2-debug <bobbiiyt.2@gmail.com>
2026-06-20 21:41:41 +0800

    Add files via upload

---
+++ JavaScript1.js
@@ -0,0 +1,24 @@
+﻿const themeButtons = document.querySelectorAll("#themeToggle");
+
+themeButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+      btn.textContent = "☀️";
+    } else {
+      btn.textContent = "🌙";
+    }
+  });
+});
+
+// simple contact form demo
+const contactForm = document.querySelector(".contact-form");
+
+if (contactForm) {
+  contactForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Message submitted successfully!");
+    contactForm.reset();
+  });
+}
